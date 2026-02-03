05d1d7fce7d62029ffe439b28a1cf9df65b77851
Markesxl <86328283+Markesxl@users.noreply.github.com>
2026-02-03 13:45:04 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'ai-planning-studio', // <--- ADICIONE ESTA LINHA AQUI
+  base: '/ai-planning-studio/', // <--- ADICIONE ESTA LINHA AQUI
   server: {
     host: "::",
     port: 8080,
